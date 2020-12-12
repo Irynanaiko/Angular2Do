744ca378d98c1d88944fb8b748f2d92475f020ea
Irynanaiko <irynaiko@ukr.net>
2020-12-12 14:56:42 +0200

    add header, footer and todo

---
+++ src/app/pages/todos/todos.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/core/interfaces';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.scss']
+})
+export class TodosComponent implements OnInit {
+  todoList: Array<Todo>;
+  constructor() { }
+
+  ngOnInit(): void {
+      this.todoList = TODO_DATA;
+  }
+
+}
+
+const TODO_DATA = [{
+  id: 1,
+  title: 'Learn JS',
+  description: '',
+  isDone: true
+}, {
+  id: 2,
+  title: 'Learn Angular',
+  description: 'Test description text',
+  isDone: false
+}
+];
